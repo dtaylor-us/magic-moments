4783b03d44b7df33a2692b75fe6f98a5d4b19826
dtaylor-us <derektaylor.us@gmail.com>
2021-02-14 02:04:52 -0600

    basic structure

--- client/src/main/kotlin/app.kt
+++ client/src/main/kotlin/app.kt
@@ -1,4 +1,3 @@
-import kotlinx.html.UL
 import react.RBuilder
 import react.RComponent
 import react.RProps
@@ -32,15 +31,15 @@ class App(props: AppProps) : RComponent<AppProps, AppState>(props) {
                             }
                         }
                         div(classes = "dtc v-mid tr pa3") {
-                            a(href = "/", classes = "f6 fw4 hover-white no-underline white-70 dn dib-ns pv2 ph3") {+"How it Works"}
-                            a(href = "/", classes = "f6 fw4 hover-white no-underline white-70 dn dib-ns pv2 ph3") {+"Pricing"}
-                            a(href = "/", classes = "f6 fw4 hover-white no-underline white-70 dn dib-l pv2 ph3") {+"About"}
-                            a(href = "/", classes = "f6 fw4 hover-white no-underline white-70 dn dib-l pv2 ph3") {+"Careers"}
-                            a(href = "/", classes = "f6 fw4 hover-white no-underline white-70 dib ml2 pv2 ph3 ba") {+"Sign Up"}
+                            a(href = "/", classes = "f6 fw4 hover-white no-underline white-70 dn dib-ns pv2 ph3") { +"How it Works" }
+                            a(href = "/", classes = "f6 fw4 hover-white no-underline white-70 dn dib-ns pv2 ph3") { +"Pricing" }
+                            a(href = "/", classes = "f6 fw4 hover-white no-underline white-70 dn dib-l pv2 ph3") { +"About" }
+                            a(href = "/", classes = "f6 fw4 hover-white no-underline white-70 dn dib-l pv2 ph3") { +"Careers" }
+                            a(href = "/", classes = "f6 fw4 hover-white no-underline white-70 dib ml2 pv2 ph3 ba") { +"Sign Up" }
                         }
                     }
                     div(classes = "tc-l mt4 mt5-m mt6-l ph3") {
-                        h1(classes = "f2 f1-l fw2 white-90 mb0 lh-title"){
+                        h1(classes = "f2 f1-l fw2 white-90 mb0 lh-title") {
                             +"This is your super impressive headline"
                         }
                         h2(classes = "fw1 f3 white-80 mt3 mb4") {
@@ -50,45 +49,65 @@ class App(props: AppProps) : RComponent<AppProps, AppState>(props) {
                             +"Call to Action"
                         }
                         span(classes = "dib v-mid ph3 white-70 mb3") { +"or" }
-                        a(href = "/", classes = "f6 no-underline grow dib v-mid white ba b--white ph3 pv2 mb3") { +"Secondary call to action"}
+                        a(href = "/", classes = "f6 no-underline grow dib v-mid white ba b--white ph3 pv2 mb3") { +"Secondary call to action" }
                     }
                 }
             }
         }
-//        navbar {
-//            attrs.bg = "dark"
-//            attrs.className = "white"
-//            attrs.variant = "dark"
-//            navbarBrand {
-//                img(src = "img/profile_circle.png", classes = "w3 h3 mr3 br-100 pa1 ba bw1 b--white") {}
-//            }
-//            div(classes = "w-75") {
-//                ul(classes = "navbar-nav") {
-//                    navItem("#", "Software Engineering")
-//                    navItem("#", "Visual Design")
-//                    navItem("#", "Mindset")
-//                }
-//                span(classes = "bi bi-alt") {}
-//
-//            }
-//
-//
-//        }
-//
-//        article(classes = "pa3 ph5-ns") {
-//            h3(classes = "f6 ttu tracked mt0") {
-//                +"Leslie is a POOP"
-//            }
-//            p(classes = "f4 lh-copy") {
-//                +"There is a base border module that can be extended with the border-width, border-color, border-style modules. By default you don’t need to set a border color. It will just inherit whatever the text color is for that dom node. You can target any side to put a border on ie. top, right, bottom, left."
-//            }
-//        }
-    }
+        section(classes = "tc-l mt3 mt4-m mt5-l mw8 center ph3") {
+            h1(classes = "f2 f1-l fw2 white-90 mb0 lh-title") {
+                +"This is your super impressive headline"
+            }
+            h2(classes = "fw1 f3 white-80 mt3 mb4") {
+                +"Now a subheadline where explain your wonderful new startup even more"
+            }
+            div(classes = "flex mt5") {
+                article(classes = "mw5 center bg-white br3 pa3 pa4-ns mv3 ba b--black-10") {
+                    div("tc") {
+                        img(src = "http://tachyons.io/img/avatar_1.jpg", classes = "br-100 h3 w3 dib") {}
+                        h1(classes = "f4 black") { +"Cat" }
+                        hr(classes = "mw3 bb bw1 b--black-10") { }
+                    }
+                    p(classes = "lh-copy measure center f6 black-70") {
+                        +"Quite affectionate and outgoing. She loves to get chin scratches and will roll around on the floor waiting for you give her more of them."
+                    }
+                }
+                article(classes = "mw5 center bg-white br3 pa3 pa4-ns mv3 ba b--black-10") {
+                    div("tc") {
+                        img(src = "http://tachyons.io/img/avatar_1.jpg", classes = "br-100 h3 w3 dib") {}
+                        h1(classes = "f4 black") { +"Cat" }
+                        hr(classes = "mw3 bb bw1 b--black-10") { }
+                    }
+                    p(classes = "lh-copy measure center f6 black-70") {
+                        +"Quite affectionate and outgoing. She loves to get chin scratches and will roll around on the floor waiting for you give her more of them."
+                    }
+                }
+                article(classes = "mw5 center bg-white br3 pa3 pa4-ns mv3 ba b--black-10") {
+                    div("tc") {
+                        img(src = "http://tachyons.io/img/avatar_1.jpg", classes = "br-100 h3 w3 dib") {}
+                        h1(classes = "f4 black") { +"Cat" }
+                        hr(classes = "mw3 bb bw1 b--black-10") { }
+                    }
+                    p(classes = "lh-copy measure center f6 black-70") {
+                        +"Quite affectionate and outgoing. She loves to get chin scratches and will roll around on the floor waiting for you give her more of them."
+                    }
+                }
+            }
 
-    private fun RDOMBuilder<UL>.navItem(url: String, text: String) {
-        li(classes = "nav-item") {
-            a(classes = "nav-link", href = url) {
-                +text
+        }
+
+        footer(classes = "pv4 mt5 ph3 ph5-m ph6-l white bg-black") {
+            small(classes = "f6 db tc") {
+                +"© 2021"
+                b(classes = "ttu") {
+                    +"SOME COMPANY Inc"
+                }
+                +"., All Rights Reserved"
+            }
+            div(classes = "tc mt3") {
+                a(href = "#", classes = "f6 dib ph2 link mid-gray dim") { +"Link1" }
+                a(href = "#", classes = "f6 dib ph2 link mid-gray dim") { +"Link2" }
+                a(href = "#", classes = "f6 dib ph2 link mid-gray dim") { +"Link3" }
             }
         }
     }
